a02c7c02912eecd19a67db2780370251d44506b3
sobanqasim <sbnqasim@gmail.com>
2025-01-17 12:54:05 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Table
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:\n");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Multiplication Table of " + number + ":\n");
+        
+
+        for (int i = 0; i <= 10; i++)
+        {
+
+            Console.WriteLine(number + " x " + i + " = " + number * i );
+        }
+      
+    }
+}
